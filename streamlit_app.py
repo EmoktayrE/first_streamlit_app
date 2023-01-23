602d78bebf9090cda8b3bce92d5d89f4f6f440d2
Emre Oktay <67924742+EmoktayrE@users.noreply.github.com>
2023-01-23 16:56:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 st.header("The fruit load list contains:")
-st.dataframe(my_data_row)
+st.dataframe(my_data_rows)
 
